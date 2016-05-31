e83c745ebbfe962eb9b7c040cdecb0c024de5b79
PengZhang13 <zhangp@mit.edu>
2016-05-31 11:10:30 -0400

    Aromatics NNI. Creat test molecules for methoxy substitution.

--- input.py
+++ input.py
@@ -365,32 +365,118 @@ species(
 )
 
 species(
-    label='testAromatics',
+    label='123456methoxy',
     structure=adjacencyList(
         """
-		1  C u0 p0 c0 {2,S} {13,S} {14,S} {15,S}
+		1  C u0 p0 c0 {2,S} {4,D} {16,S}
 		2  O u0 p2 c0 {1,S} {3,S}
-		3  C u0 p0 c0 {2,S} {4,D} {11,S}
-		4  C u0 p0 c0 {3,D} {5,S} {16,S}
-		5  C u0 p0 c0 {4,S} {6,S} {7,D}
-		6  C u0 p0 c0 {5,S} {17,S} {18,S} {19,S}
-		7  C u0 p0 c0 {5,D} {8,S} {9,S}
-		8  C u0 p0 c0 {7,S} {20,S} {21,S} {22,S}
-		9  C u0 p0 c0 {7,S} {10,S} {11,D}
-		10 O u0 p2 c0 {9,S} {23,S}
-		11 C u0 p0 c0 {3,S} {9,D} {12,S}
-		12 O u0 p2 c0 {11,S} {24,S}
-		13 H u0 p0 c0 {1,S}
-		14 H u0 p0 c0 {1,S}
-		15 H u0 p0 c0 {1,S}
-		16 H u0 p0 c0 {4,S}
-		17 H u0 p0 c0 {6,S}
-		18 H u0 p0 c0 {6,S}
-		19 H u0 p0 c0 {6,S}
-		20 H u0 p0 c0 {8,S}
-		21 H u0 p0 c0 {8,S}
-		22 H u0 p0 c0 {8,S}
-		23 H u0 p0 c0 {10,S}
-		24 H u0 p0 c0 {12,S}
+		3  C u0 p0 c0 {2,S} {19,S} {20,S} {21,S}
+		4  C u0 p0 c0 {1,D} {5,S} {7,S}
+		5  O u0 p2 c0 {4,S} {6,S}
+		6  C u0 p0 c0 {5,S} {22,S} {23,S} {24,S}
+		7  C u0 p0 c0 {4,S} {8,S} {10,D}
+		8  O u0 p2 c0 {7,S} {9,S}
+		9  C u0 p0 c0 {8,S} {25,S} {26,S} {27,S}
+		10 C u0 p0 c0 {7,D} {11,S} {13,S}
+		11 O u0 p2 c0 {10,S} {12,S}
+		12 C u0 p0 c0 {11,S} {28,S} {29,S} {30,S}
+		13 C u0 p0 c0 {10,S} {14,S} {16,D}
+		14 O u0 p2 c0 {13,S} {15,S}
+		15 C u0 p0 c0 {14,S} {31,S} {32,S} {33,S}
+		16 C u0 p0 c0 {1,S} {13,D} {17,S}
+		17 O u0 p2 c0 {16,S} {18,S}
+		18 C u0 p0 c0 {17,S} {34,S} {35,S} {36,S}
+		19 H u0 p0 c0 {3,S}
+		20 H u0 p0 c0 {3,S}
+		21 H u0 p0 c0 {3,S}
+		22 H u0 p0 c0 {6,S}
+		23 H u0 p0 c0 {6,S}
+		24 H u0 p0 c0 {6,S}
+		25 H u0 p0 c0 {9,S}
+		26 H u0 p0 c0 {9,S}
+		27 H u0 p0 c0 {9,S}
+		28 H u0 p0 c0 {12,S}
+		29 H u0 p0 c0 {12,S}
+		30 H u0 p0 c0 {12,S}
+		31 H u0 p0 c0 {15,S}
+		32 H u0 p0 c0 {15,S}
+		33 H u0 p0 c0 {15,S}
+		34 H u0 p0 c0 {18,S}
+		35 H u0 p0 c0 {18,S}
+		36 H u0 p0 c0 {18,S}
+        """),
+)
+
+species(
+    label='1234methoxy_56OH',
+    structure=adjacencyList(
+        """
+		1  C u0 p0 c0 {2,S} {4,D} {15,S}
+		2  O u0 p2 c0 {1,S} {3,S}
+		3  C u0 p0 c0 {2,S} {17,S} {18,S} {19,S}
+		4  C u0 p0 c0 {1,D} {5,S} {7,S}
+		5  O u0 p2 c0 {4,S} {6,S}
+		6  C u0 p0 c0 {5,S} {20,S} {21,S} {22,S}
+		7  C u0 p0 c0 {4,S} {8,S} {10,D}
+		8  O u0 p2 c0 {7,S} {9,S}
+		9  C u0 p0 c0 {8,S} {23,S} {24,S} {25,S}
+		10 C u0 p0 c0 {7,D} {11,S} {13,S}
+		11 O u0 p2 c0 {10,S} {12,S}
+		12 C u0 p0 c0 {11,S} {26,S} {27,S} {28,S}
+		13 C u0 p0 c0 {10,S} {14,S} {15,D}
+		14 O u0 p2 c0 {13,S} {29,S}
+		15 C u0 p0 c0 {1,S} {13,D} {16,S}
+		16 O u0 p2 c0 {15,S} {30,S}
+		17 H u0 p0 c0 {3,S}
+		18 H u0 p0 c0 {3,S}
+		19 H u0 p0 c0 {3,S}
+		20 H u0 p0 c0 {6,S}
+		21 H u0 p0 c0 {6,S}
+		22 H u0 p0 c0 {6,S}
+		23 H u0 p0 c0 {9,S}
+		24 H u0 p0 c0 {9,S}
+		25 H u0 p0 c0 {9,S}
+		26 H u0 p0 c0 {12,S}
+		27 H u0 p0 c0 {12,S}
+		28 H u0 p0 c0 {12,S}
+		29 H u0 p0 c0 {14,S}
+		30 H u0 p0 c0 {16,S}
+        """),
+)
+
+species(
+    label='12methoxy_34vinyl_56OH',
+    structure=adjacencyList(
+        """
+		1  C u0 p0 c0 {2,S} {4,D} {15,S}
+		2  O u0 p2 c0 {1,S} {3,S}
+		3  C u0 p0 c0 {2,S} {17,S} {18,S} {19,S}
+		4  C u0 p0 c0 {1,D} {5,S} {7,S}
+		5  O u0 p2 c0 {4,S} {6,S}
+		6  C u0 p0 c0 {5,S} {20,S} {21,S} {22,S}
+		7  C u0 p0 c0 {4,S} {8,S} {10,D}
+		8  C u0 p0 c0 {7,S} {9,D} {23,S}
+		9  C u0 p0 c0 {8,D} {24,S} {25,S}
+		10 C u0 p0 c0 {7,D} {11,S} {13,S}
+		11 C u0 p0 c0 {10,S} {12,D} {26,S}
+		12 C u0 p0 c0 {11,D} {27,S} {28,S}
+		13 C u0 p0 c0 {10,S} {14,S} {15,D}
+		14 O u0 p2 c0 {13,S} {29,S}
+		15 C u0 p0 c0 {1,S} {13,D} {16,S}
+		16 O u0 p2 c0 {15,S} {30,S}
+		17 H u0 p0 c0 {3,S}
+		18 H u0 p0 c0 {3,S}
+		19 H u0 p0 c0 {3,S}
+		20 H u0 p0 c0 {6,S}
+		21 H u0 p0 c0 {6,S}
+		22 H u0 p0 c0 {6,S}
+		23 H u0 p0 c0 {8,S}
+		24 H u0 p0 c0 {9,S}
+		25 H u0 p0 c0 {9,S}
+		26 H u0 p0 c0 {11,S}
+		27 H u0 p0 c0 {12,S}
+		28 H u0 p0 c0 {12,S}
+		29 H u0 p0 c0 {14,S}
+		30 H u0 p0 c0 {16,S}
         """),
 )
